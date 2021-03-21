b75c36141fd14e4d5cfedf31aab40691585891d9
Jasmine Garcia <garciajasminejoy@gmail.com>
2021-03-21 20:21:49 +0800

    update modal service

--- resources/frontend/src/app/core/services/modal.service.ts
+++ resources/frontend/src/app/core/services/modal.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { BsModalRef } from 'ngx-bootstrap/modal';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { PushNotificationService } from './push-notification.service';
+import { Modals } from 'src/app/shared/enums/modals.enum';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -9,7 +12,15 @@ export class ModalService {
     private closeModal: BehaviorSubject<boolean> = new BehaviorSubject(true);
     refs: BsModalRef[] = [];
 
-    constructor() {}
+    modalQueue: BehaviorSubject<number[]> = new BehaviorSubject<number[]>([]);
+    currentOpenModal: number;
+    namedModalRefs: any = {};
+    queueSub: Subscription;
+
+    constructor(
+        private pushNotifService: PushNotificationService,
+        private bsModalService: BsModalService
+    ) {}
 
     closeAll(): void {
         this.refs.forEach(ref => {
@@ -40,5 +51,125 @@ export class ModalService {
         ) as HTMLElement;
         base.style.width = parseFloat(ratioWidth.toString()) * formula + 'px';
         base.style.height = parseFloat(ratioHeight.toString()) * formula + 'px';
+    }
+
+    init(): void {
+        this.listenToQueue();
+    }
+
+    kill(): void {
+        if (!!this.queueSub) {
+            this.queueSub.unsubscribe();
+        }
+    }
+
+    listenToQueue(): void {
+        this.queueSub = this.getModalQueue$()
+            .pipe(
+            debounceTime(500), // necessary para makuha muna lahat ng modals na i-queue
+            distinctUntilChanged()
+        )
+        .subscribe(queue => {
+            if (queue.length && this.currentOpenModal !== queue[0]) {
+                this.currentOpenModal = queue[0];
+                switch (queue[0]) {
+                case Modals.LIVESTREAM:
+                    this.openHallStatusModal();
+                    break;
+                case Modals.PUSH:
+                    this.openPushNotification();
+                    break;
+                case Modals.WELCOME:
+                    this.openWelcomeModal();
+                    break;
+                }
+            } else {
+                this.currentOpenModal = null;
+            }
+        });
+    }
+
+    getModalQueue$(): Observable<number[]> {
+        return this.modalQueue.asObservable();
+    }
+
+    addToQueue(queueItem: number): void {
+        const items = this.modalQueue.getValue();
+        if (items.includes(queueItem)) {
+            return;
+        }
+        this.setQueue([...items, queueItem]);
+    }
+
+    dequeue(queueName: number): void {
+        this.closeGlobalModal(this.getModalKey(queueName));
+
+        const queue = this.modalQueue.getValue();
+        const newValue = queue.filter(item => item !== queueName);
+        this.setQueue(newValue);
+        }
+
+    setQueue(items: number[]): void {
+        const orderedItems = items.sort();
+        this.modalQueue.next(orderedItems);
+    }
+
+    closeGlobalModal(modalName: string): void {
+        if (this.namedModalRefs[modalName]) {
+            this.namedModalRefs[modalName].hide();
+            delete this.namedModalRefs[modalName];
+        }
+    }
+
+    closeAllOpenModals(): void {
+        Object.keys(this.namedModalRefs).forEach(modalKey => {
+            if (this.namedModalRefs.hasOwnProperty(modalKey)) {
+                this.closeGlobalModal(modalKey);
+            }
+        });
+    }
+
+    isNameModalOpen(modalName: string): boolean {
+        return this.namedModalRefs[modalName];
+    }
+
+    killAll(): void {
+        this.currentOpenModal = null;
+        this.setQueue([]);
+        this.pushNotifService.toggle(false);
+        this.closeAllOpenModals();
+    }
+
+    openHallStatusModal(): void {
+        // TODO: call hall status modal
+        // this.namedModalRefs['livestream'] = 
+    }
+
+    private openPushNotification(): void {
+        if (this.namedModalRefs['push']) {
+            return;
+        }
+
+        // TODO: call push notif modal
+        // this.namedModalRefs['push'] = 
+    }
+
+    private openWelcomeModal(): void {
+        if (this.namedModalRefs['welcome']) {
+            return;
+        }
+    
+        // TODO: call welcome modal
+        this.namedModalRefs['welcome'] = 
+    }
+
+    private getModalKey(modalOrder: number): string {
+        const modalNames = {
+          [Modals.WELCOME]: 'welcome',
+          [Modals.PUSH]: 'push',
+          [Modals.LIVESTREAM]: 'livestream',
+          [Modals.SPOUSE_WORKSHOP]: 'workshop'
+        };
+        return modalNames[modalOrder];
     }
 }
---
+++ resources/frontend/src/app/shared/enums/modals.enum.ts
@@ -0,0 +1,6 @@
+export enum Modals {
+  WELCOME = 1,
+  PUSH = 2,
+  LIVESTREAM = 3,
+  SPOUSE_WORKSHOP = 4
+}
