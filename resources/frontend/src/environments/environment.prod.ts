07d2c78912c0fba6cf9a773d3298fd3dc0e465e9
Jasmine Garcia <garciajasminejoy@gmail.com>
2021-03-23 21:25:55 +0800

    environment configuration

--- resources/frontend/src/environments/environment.prod.ts
+++ resources/frontend/src/environments/environment.prod.ts
@@ -1,3 +1,22 @@
 export const environment = {
-  production: true
+  production: true,
+  talkJs: {
+    appId: 'tkcoPA06',
+    secretKey: 'sk_test_KgCO7bGedkypkQ15rwoJnqPP',
+    userIdPrefix: 'app-prod-talk'
+  },
+  api: {
+      endpoint: 'http://localhost:8000',
+  },
+  pusher: {
+      id: 'waveplay',
+      key: 'interactive',
+      secret: 'local',
+      cluster: 'asia',
+      port: 6003,
+      scheme: 'http',
+      // scheme: 'https',
+      host: 'localhost',
+      // host: '',
+  }
 };
--- resources/frontend/src/environments/environment.ts
+++ resources/frontend/src/environments/environment.ts
@@ -4,9 +4,25 @@
 
 export const environment = {
 	production: false,
-	api: {
-		endpoint: 'http://localhost:8000/api/v1'
-	}
+    talkJs: {
+        appId: 'tkcoPA06',
+        secretKey: 'sk_test_KgCO7bGedkypkQ15rwoJnqPP',
+        userIdPrefix: 'app-prod-talk'
+    },
+    api: {
+        endpoint: 'http://localhost:8000',
+    },
+    pusher: {
+        id: 'waveplay',
+        key: 'interactive',
+        secret: 'local',
+        cluster: 'asia',
+        port: 6003,
+        scheme: 'http',
+        // scheme: 'https',
+        host: 'localhost',
+        // host: '',
+    },
 };
 
 /*
