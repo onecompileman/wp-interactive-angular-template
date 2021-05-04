9df626f7f8c49add11c7faf54a17698ea9842dae
Jasmine Garcia <garciajasminejoy@gmail.com>
2021-05-04 21:37:08 +0800

    configure websocket service

--- resources/frontend/src/app/core/services/websocket.service.ts
+++ resources/frontend/src/app/core/services/websocket.service.ts
@@ -40,78 +40,81 @@ export class WebsocketService {
         },
       });
 
+      const appPrefix = environment.appPrefix;
+
       window['currentLocation'] = 'loading';
-      // let cname = 'U' + btoa(localStorage.getItem('globe.user-id').padStart(5));
-      // window['Echo'].private(cname);
-      let cname = 'TEST';
-      window['Echo'].channel(cname);
+      let cname = 'U' + btoa(localStorage.getItem(`${appPrefix}.user-id`).padStart(5));
+      window['Echo'].private(cname);
+      // let cname = 'TEST';
+      // window['Echo'].channel(cname);
 
       window['changeLocation'] = (location, channel) => {
           if (channel != window['currentChannel'] && channel){
-              // let previousChannel = `private-${window['currentChannel']}`;
-              let previousChannel = window['currentChannel'];
+              let previousChannel = `private-${window['currentChannel']}`;
+              // let previousChannel = window['currentChannel'];
               window['currentChannel'] = channel;
               window['Echo'].leave(previousChannel);
-              // window['Echo'].private(channel).listen('SystemEvent', this.eventCallback);
-              window['Echo'].channel(channel).listen('SystemEvent', this.eventCallback);
+              window['Echo'].private(channel).listen('SystemEvent', this.eventCallback);
+              // window['Echo'].channel(channel).listen('SystemEvent', this.eventCallback);
           }
           window['currentLocation'] = location;
           // window['clientEvent'](location);
           // window['clientVisit']();
       }
 
-      // window['clientPing'] = () => {
-      //   let wsSuccess = false;
-      //   wsSuccess = window['Echo'].connector.pusher.channels.channels[
-      //     `private-${cname}`
-      //     // `${cname}`
-      //   ].trigger('client-ping', {
-      //     U: localStorage.getItem('globe.user-id'),
-      //     L: window['currentLocation'],
-      //   });
-      //   if (!wsSuccess) {
-      //     this.analyticsDataService.pushActivity({
-      //       L: window['currentLocation'],
-      //       _token: document
-      //         .querySelector('meta[name="csrf"]')
-      //         .getAttribute('content'),
-      //     });
-      //   }
-      // };
+      window['clientPing'] = () => {
+        let wsSuccess = false;
+        wsSuccess = window['Echo'].connector.pusher.channels.channels[
+          `private-${cname}`
+          // `${cname}`
+        ].trigger('client-ping', {
+          U: localStorage.getItem(`${appPrefix}.user-id`),
+          L: window['currentLocation'],
+        });
+        if (!wsSuccess) {
+          this.analyticsDataService.pushActivity({
+            L: window['currentLocation'],
+            _token: document
+              .querySelector('meta[name="csrf"]')
+              .getAttribute('content'),
+          });
+        }
+      };
 
-      // setInterval(() => {
-      //   window['clientPing']();
-      // }, 60000 / 3); // original -> 180000
+      setInterval(() => {
+        window['clientPing']();
+      }, 60000 / 3); // original -> 180000
 
-      // window['clientVisit'] = (visit_location = false) => {
-      //   let visit = window['currentLocation'];
-      //   if (visit_location) {
-      //     visit = visit_location;
-      //   }
-      //   let wsSuccess = false;
-      //   wsSuccess = window['Echo'].connector.pusher.channels.channels[
-      //     `private-${cname}`
-      //   ].trigger('client-visit', {
-      //     U: localStorage.getItem('globe.user-id'),
-      //     L: visit,
-      //   });
-      //   if (!wsSuccess) {
-      //     this.analyticsDataService.pushActivity({
-      //       L: visit,
-      //       _token: document
-      //         .querySelector('meta[name="csrf"]')
-      //         .getAttribute('content'),
-      //     });
-      //   }
-      // };
+      window['clientVisit'] = (visit_location = false) => {
+        let visit = window['currentLocation'];
+        if (visit_location) {
+          visit = visit_location;
+        }
+        let wsSuccess = false;
+        wsSuccess = window['Echo'].connector.pusher.channels.channels[
+          `private-${cname}`
+          // `${cname}`
+        ].trigger('client-visit', {
+          U: localStorage.getItem(`${appPrefix}.user-id`),
+          L: visit,
+        });
+        if (!wsSuccess) {
+          this.analyticsDataService.pushActivity({
+            L: visit,
+            _token: document
+              .querySelector('meta[name="csrf"]')
+              .getAttribute('content'),
+          });
+        }
+      };
 
       window['clientEvent'] = (category, action) => {
         let wsSuccess = false;
         wsSuccess = window['Echo'].connector.pusher.channels.channels[
-          // `private-${cname}`
-          `${cname}`
+          `private-${cname}`
+          // `${cname}`
         ].trigger('client-event', {
-          U: localStorage.getItem('globe.user-id'),
+          U: localStorage.getItem(`${appPrefix}.user-id`),
           L: category,
           A: action,
         });
