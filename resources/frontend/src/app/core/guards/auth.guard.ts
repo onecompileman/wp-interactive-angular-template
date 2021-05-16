19176e465002deffce01043681935a87e3bf801c
Jasmine Garcia <garciajasminejoy@gmail.com>
2021-05-16 19:36:41 +0800

    auth guard

---
+++ resources/frontend/src/app/core/guards/auth.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+    ActivatedRouteSnapshot, CanActivate,
+    Router, RouterStateSnapshot,
+    UrlTree
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+
+    constructor(private router: Router) {}
+
+    canActivate(
+        next: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ):
+        | Observable<boolean | UrlTree>
+        | Promise<boolean | UrlTree>
+        | boolean
+        | UrlTree {
+
+        if (localStorage.getItem('tvf.user-id')) {
+            return true;
+        }
+
+        return this.router.parseUrl('tv5');
+    }
+}
